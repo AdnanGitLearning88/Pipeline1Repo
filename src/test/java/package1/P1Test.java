1e61a9b36fce56d51b3e9f8fbdc5776e0546744a
adshaikh <adshaikh@INAILABNMS26.ecitele.com>
2022-01-11 19:53:13 +0530

    Added Test Method

--- src/test/java/package1/P1Test.java
+++ src/test/java/package1/P1Test.java
@@ -1,5 +1,14 @@
 package package1;
 
+import org.testng.annotations.Test;
+
 public class P1Test {
 
+	@Test
+	public void testP2() throws Exception {
+		
+		System.out.println("Executing Test in Pipeline 1");
+		
+	}
+	
 }
